3dca5c2956725d49c3752bd42e8518bbf9f78f9e
caozhen <colin099@126.com>
2016-05-19 13:32:47 +0800

    creat a empty project

---
+++ CZPrivateProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZPrivateProject
+//
+//  Created by caozhen@neusoft on 16/5/19.
+//  Copyright © 2016年 Neusoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CZPrivateProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CZPrivateProject
+//
+//  Created by caozhen@neusoft on 16/5/19.
+//  Copyright © 2016年 Neusoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
